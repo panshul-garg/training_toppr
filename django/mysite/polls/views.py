972173850fadc4e09d40d9b10647c18b1cfffff8
Panshul Garg <panshul.garg@toppr.com>
2018-06-25 18:43:49 +0530

    Basic of Django

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 # Create your views here.
 def index(request):
 	print ("\n\n\n\n")
 	print (request)
 	print ("\n\n\n\n")
-	return HttpResponse("Hello World")
+	latest_question_list=Question.objects.order_by('-pub_date')[:5]
+	template=loader.get_template("polls/index.html")
+	context={
+		'latest_question_list':latest_question_list,
+	}
+	return HttpResponse(template.render(context,request))
 
+def detail(request,question_id):
+	print ("\n\n\n\n")
+	print (request)
+	print (question_id)
+	print ("\n\n\n\n") 
+	return HttpResponse("You are looking at question %s." %question_id)
+
+
+def results(request,question_id):
+	print ("\n\n\n\n")
+	print (request)
+	print (question_id)
+	print ("\n\n\n\n")
+	response= "You are looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+	print ("\n\n\n\n")
+	print (request)
+	print (question_id)
+	print ("\n\n\n\n")
+	return HttpResponse("You are voting on question %s" % question_id )
