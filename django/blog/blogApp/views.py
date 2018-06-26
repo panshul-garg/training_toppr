bf91bcebf88d5af5a03f9d4fbf1ccc312dbf3fb3
Panshul Garg <panshul.garg@toppr.com>
2018-06-26 17:49:49 +0530

    Blog App Initial Commit

---
+++ django/blog/blogApp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render , get_object_or_404
+from .models import Post
+
+
+# Create your views here.
+
+def post_list(request):
+	posts_list=Post.published.all()
+
+	print ( "\n\n\n\n" )
+	print ( "We have a request" )
+	print ( "Scheme  " + str(request.scheme) )
+	print ( "Body  " + str( request.body ) )
+	print ( "Path " + str( request.path) )
+	print ( "Path_Info " + str( request.path_info ))
+	print ( "Method " + str  (request.method) )
+	print ( "Get" + str (request.GET ) )
+	for post in posts_list:
+		print (post.get_absolute_url())
+	print ( "\n\n\n\n" )
+
+
+	return render( request , 'blogApp/post/list.html' , { 'posts' : posts_list } )
+
+
+
+def post_detail( request, year , month , day , post ):
+	post = get_object_or_404( Post , slug = post , status = 'published' , publish__year = year , publish__month = month , publish__day = day )
+	return render( request , 'detail.html' , { 'post' : post } )
